72d060bce6c68445592f2d99d9f9fa510fb574d7
Aidai <150067646+git-aidai-kydykbekova@users.noreply.github.com>
2024-09-13 16:49:13 +0600

    Add files via upload
    add 4th task

--- zero.f90
+++ zero.f90
@@ -1,48 +1,46 @@
 !task1
-! program epsilon
-!   implicit none
-!   real :: eps
-!   integer :: n 
-!   n = 0
-!   eps = 1.0
-!     do
-!       eps = eps / 10 
-!       n = n + 1
-!       if(1 + eps <= 1) then 
-!         exit
-!       end if
-!     end do
+program epsilon
+implicit none
+real :: eps
+integer :: n 
+n = 0
+eps = 1.0
+do
+  eps = eps / 10 
+  n = n + 1
+  if(1 + eps <= 1) then 
+    exit
+  end if
+end do
   
-!   print *, 'n: ', n
-!   print *, 'eps: ', eps
+print *, 'n: ', n
+print *, 'eps: ', eps
 
-
-! end program epsilon
+end program epsilon
 
 
 
 
 !task2
 
-! program epsilon
-!   implicit none
-!   real(8) :: eps
-!   integer :: n 
-!   n = 0
-!   eps = 1.0
-!     do
-!       eps = eps / 10 
-!       n = n + 1
-!       if(1 + eps <= 1) then 
-!         exit
-!       end if
-!     end do
-  
-!   print *, 'n: ',n
-!   print *, 'eps: ',eps
+program epsilon
+implicit none
+real(8) :: eps
+integer :: n 
+n = 0
+eps = 1.0
+do
+  eps = eps / 10 
+  n = n + 1
+  if(1 + eps <= 1) then 
+    exit
+  end if
+end do
 
+print *, 'n: ',n
+print *, 'eps: ',eps
 
-! end program epsilon
+end program epsilon
 
 
 
@@ -61,13 +59,36 @@ do while(2*z>z)
   b = b+1
   z= z/2
 end do 
+
 print *, 'last zero', last_z
 print *, 'zero',z
 print *, 'b', b
-End program zero
+
+end program zero
 
 
 
 !task4
+program infinity
+implicit none 
+real(8) :: inf
+integer :: n
+
+n = 0
+inf = 1.0
+do 
+  n=n+1
+  inf = 10 * inf
+  if(2*inf == inf) then 
+    exit
+  end if 
+end do
+
+print *, 'inf: ', inf
+print *, 'n: ', n
+end program infinity
+
+
+
 
 

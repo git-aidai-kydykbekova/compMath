1f35def3f9e6164cac1b7ecc556a851663c0238c
Aidai <150067646+git-aidai-kydykbekova@users.noreply.github.com>
2024-09-13 17:48:55 +0600

    Add files via upload

--- zero.f90
+++ zero.f90
@@ -1,46 +1,46 @@
 !task1
-program epsilon
-implicit none
-real :: eps
-integer :: n 
-n = 0
-eps = 1.0
-do
-  eps = eps / 10 
-  n = n + 1
-  if(1 + eps <= 1) then 
-    exit
-  end if
-end do
+! program epsilon
+! implicit none
+! real :: eps
+! integer :: n 
+! n = 0
+! eps = 1.0
+! do
+!   eps = eps / 10 
+!   n = n + 1
+!   if(1 + eps <= 1) then 
+!     exit
+!   end if
+! end do
   
-print *, 'n: ', n
-print *, 'eps: ', eps
+! write (*,*)  'n: ', n
+! write (*,*)  'eps: ', eps
 
-end program epsilon
+! end program epsilon
 
 
 
 
 !task2
 
-program epsilon
-implicit none
-real(8) :: eps
-integer :: n 
-n = 0
-eps = 1.0
-do
-  eps = eps / 10 
-  n = n + 1
-  if(1 + eps <= 1) then 
-    exit
-  end if
-end do
+! program epsilon
+! implicit none
+! real(8) :: eps
+! integer :: n 
+! n = 0
+! eps = 1.0
+! do
+!   eps = eps / 10 
+!   n = n + 1
+!   if(1 + eps <= 1) then 
+!     exit
+!   end if
+! end do
 
-print *, 'n: ',n
-print *, 'eps: ',eps
+! write (*,*)  'n: ',n
+! write (*,*)  'eps: ',eps
 
-end program epsilon
+! end program epsilon
 
 
 
@@ -54,39 +54,42 @@ integer ::  b
 real :: last_Z
 b = 0
 z = 1.0
-do while(2*z>z)
+do 
   last_z=z
   b = b+1
   z= z/2
+  if(2*z==z) then 
+    exit 
+  end if
 end do 
 
-print *, 'last zero', last_z
-print *, 'zero',z
-print *, 'b', b
+write (*,*) 'last zero', last_z
+write (*,*) 'zero',z
+write (*,*) 'b', b
 
 end program zero
 
 
 
 !task4
-program infinity
-implicit none 
-real(8) :: inf
-integer :: n
+! program infinity
+! implicit none 
+! real(8) :: inf
+! integer :: n
 
-n = 0
-inf = 1.0
-do 
-  n=n+1
-  inf = 10 * inf
-  if(2*inf == inf) then 
-    exit
-  end if 
-end do
+! n = 0
+! inf = 1.0
+! do 
+!   n=n+1
+!   inf = 10 * inf
+!   if(2*inf == inf) then 
+!     exit
+!   end if 
+! end do
 
-print *, 'inf: ', inf
-print *, 'n: ', n
-end program infinity
+! write (*,*)  'inf: ', inf
+! write (*,*)  'n: ', n
+! end program infinity
 
 
 

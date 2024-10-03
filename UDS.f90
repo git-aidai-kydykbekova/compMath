d6f456efb1fff216234652c2b52ac1d831ae4165
Aidai <150067646+git-aidai-kydykbekova@users.noreply.github.com>
2024-10-03 15:48:43 +0600

    Add files via upload
    update program for Darbu's Sum

--- UDS.f90
+++ UDS.f90
@@ -1,8 +1,8 @@
 
 Program UDS
 
-integer inputNum, counter;
-real a, b, weith, height, x, area;
+integer inputNum, counter, counter2;
+real a, b, weith, height, x, area, LSDArea, darbuSum;
 Write(*,*) "Input write:";
 Read *, inputNum;
 
@@ -10,21 +10,70 @@ Read *, inputNum;
 counter = 0;
 
 
-a = 4.d0 * datan(1.d0);
-b = 1.5 * a;
+b = 4.d0 * datan(1.d0);
+a = 0.0;
 
 area = 0.0;
 
 weith = (b-a)/inputNum;
-x = a + weith;
+x = weith;
 
 Do while(counter < inputNum)
+
+if(sin(x) > sin(x-weith)) then
 height = sin(x);
+end if;
+
+
+
+if(sin(x) < sin(x-weith)) then
+height = sin(x-weith);
+end if;
+
+
+
+
 x = x + weith;
 area = area + height * weith;
 counter = counter + 1;
+
+if(inputNum == 1) then
+area = weith * 1;
+end if;
 End do;
 
-Write(*,*) "LDS",area;
+Do while(counter2 < inputNum)
+
+if(sin(x) > sin(x-weith)) then
+height = sin(x-weith);
+end if;
+
+
+
+if(sin(x) < sin(x-weith)) then
+height = sin(x);
+end if;
+
+
+
+
+x = x + weith;
+LSDArea = LSDArea + height * weith;
+counter2 = counter2 + 1;
+
+if(inputNum == 1) then
+LSDArea = weith * 0;
+end if;
+End do;
+
+
+darbuSum = (area + LSDArea)/2;
+
+
+
+Write(*,*) "UDS",area;
+Write(*,*) "LDS",LSDArea;
+Write(*,*) "darbuSum",darbuSum;
+
 
 End Program UDS
